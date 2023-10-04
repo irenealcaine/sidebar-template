71fbdd102eea1622f848d44d1160dbb3a0a4d097
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-10-04 08:12:21 +0000

    initial commit, navbar

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { navbarItems } from "../../Utils/Constants";
+import "./Footer.scss";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../Context/darkModeContext";
+
+const Footer = () => {
+  const { dispatch, darkMode } = useContext(DarkModeContext);
+
+  return (
+    <nav className={` footer ${darkMode ? "dark" : ""}`}>
+      <ul className="footer-list">
+        {navbarItems.map((navbarItem) => (
+          <li key={navbarItem.slug} className="footer-item">
+            <Link to={`/${navbarItem.slug}`}>{navbarItem.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="color-options">
+        <div
+          className="color-option"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        >
+          ⚪
+        </div>
+        <div
+          className="color-option"
+          onClick={() => dispatch({ type: "DARK" })}
+        >
+          ⚫
+        </div>
+        <div
+          className="color-option"
+          onClick={() => dispatch({ type: "TOGGLE" })}
+        >
+          {darkMode ? "☀️" : "🌙"}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import { useContext } from "react";
+import "./Header.scss";
+import logo from "../../Assets/Images/logo.png";
+import { Link, useLocation } from "react-router-dom";
+import { navbarItems } from "../../Utils/Constants";
+import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { DarkModeContext } from "../../Context/darkModeContext";
+
+const Header = () => {
+  const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuActiveClass = isMenuOpen ? "active" : "";
+  const { darkMode } = useContext(DarkModeContext);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className={`header ${darkMode ? "dark" : ""}`}>
+      <Link to={"/"} onClick={() => setIsMenuOpen(false)} className="logo-link">
+        <img src={logo} alt={"Logo"} className="logo" />
+      </Link>
+
+      <ul className={`navigation ${menuActiveClass} ${darkMode ? "dark" : ""}`}>
+        {navbarItems.map((navbarItem) => (
+          <li key={navbarItem.slug} onClick={() => setIsMenuOpen(false)}>
+            <Link
+              to={`/${navbarItem.slug}`}
+              className={`navigation-item ${
+                location.pathname === `/${navbarItem.slug}` && "active"
+              }`}
+            >
+              {navbarItem.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className={`menu-toggle ${menuActiveClass} `} onClick={toggleMenu}>
+        {isMenuOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
