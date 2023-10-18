08a6ce4508a89185e1321cac65fb14b91fb70fde
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-10-18 11:58:05 +0000

    Update 6 files

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import Button from "../../Components/Button/Button";
+import Loader from "../../Components/Loader/Loader";
 import "./Home.scss";
 
 const Home = () => {
@@ -11,19 +13,8 @@ const Home = () => {
         numquam ea, quo quibusdam. Voluptatibus velit tempora sed veritatis
         porro numquam repellat cum delectus. Aliquid doloremque aperiam rem
         accusamus ut, veniam velit, amet dolore, at libero voluptatem iure?
-        Obcaecati cum quo alias temporibus consequuntur. Culpa suscipit quod
-        saepe itaque magnam deserunt maiores. Ipsum quod aperiam, beatae, porro
-        doloremque voluptate, animi excepturi doloribus minima ea perferendis!
-        Minima odit enim officiis commodi iste nostrum ipsam placeat! Rerum
-        reprehenderit molestiae ut sint modi? Vel, esse suscipit. Ducimus
-        quaerat omnis ullam? Labore corrupti laudantium dicta assumenda itaque
-        vitae voluptas aliquid ratione ducimus accusamus unde quas provident,
-        commodi a? Architecto incidunt mollitia quam est, nisi sit, esse ipsam
-        alias eius consequuntur neque. Soluta eos quisquam cumque ad esse
-        officiis, deserunt vitae autem nisi placeat quos itaque fugiat. Quis,
-        unde!
       </p>
-      <h2>Subtitle 1</h2>
+      <h2>Paragraph</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
         placeat a explicabo? Molestiae error, minus libero dolorem eveniet
@@ -31,60 +22,18 @@ const Home = () => {
         numquam ea, quo quibusdam. Voluptatibus velit tempora sed veritatis
         porro numquam repellat cum delectus. Aliquid doloremque aperiam rem
         accusamus ut, veniam velit, amet dolore, at libero voluptatem iure?
-        Obcaecati cum quo alias temporibus consequuntur. Culpa suscipit quod
-        saepe itaque magnam deserunt maiores. Ipsum quod aperiam, beatae, porro
-        doloremque voluptate, animi excepturi doloribus minima ea perferendis!
-        Minima odit enim officiis commodi iste nostrum ipsam placeat! Rerum
-        reprehenderit molestiae ut sint modi? Vel, esse suscipit. Ducimus
-        quaerat omnis ullam? Labore corrupti laudantium dicta assumenda itaque
-        vitae voluptas aliquid ratione ducimus accusamus unde quas provident,
-        commodi a? Architecto incidunt mollitia quam est, nisi sit, esse ipsam
-        alias eius consequuntur neque. Soluta eos quisquam cumque ad esse
-        officiis, deserunt vitae autem nisi placeat quos itaque fugiat. Quis,
-        unde!
       </p>
-      <h2>Subtitle 2</h2>
-      <h3>Subsubtitle 1</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
-        placeat a explicabo? Molestiae error, minus libero dolorem eveniet
-        mollitia accusantium voluptate voluptates ipsa pariatur itaque ratione
-        numquam ea, quo quibusdam. Voluptatibus velit tempora sed veritatis
-        porro numquam repellat cum delectus. Aliquid doloremque aperiam rem
-        accusamus ut, veniam velit, amet dolore, at libero voluptatem iure?
-        Obcaecati cum quo alias temporibus consequuntur. Culpa suscipit quod
-        saepe itaque magnam deserunt maiores. Ipsum quod aperiam, beatae, porro
-        doloremque voluptate, animi excepturi doloribus minima ea perferendis!
-        Minima odit enim officiis commodi iste nostrum ipsam placeat! Rerum
-        reprehenderit molestiae ut sint modi? Vel, esse suscipit. Ducimus
-        quaerat omnis ullam? Labore corrupti laudantium dicta assumenda itaque
-        vitae voluptas aliquid ratione ducimus accusamus unde quas provident,
-        commodi a? Architecto incidunt mollitia quam est, nisi sit, esse ipsam
-        alias eius consequuntur neque. Soluta eos quisquam cumque ad esse
-        officiis, deserunt vitae autem nisi placeat quos itaque fugiat. Quis,
-        unde!
-      </p>
-      <h2>Subtitle 3</h2>
-      <h3>Subsubtitle 2</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
-        placeat a explicabo? Molestiae error, minus libero dolorem eveniet
-        mollitia accusantium voluptate voluptates ipsa pariatur itaque ratione
-        numquam ea, quo quibusdam. Voluptatibus velit tempora sed veritatis
-        porro numquam repellat cum delectus. Aliquid doloremque aperiam rem
-        accusamus ut, veniam velit, amet dolore, at libero voluptatem iure?
-        Obcaecati cum quo alias temporibus consequuntur. Culpa suscipit quod
-        saepe itaque magnam deserunt maiores. Ipsum quod aperiam, beatae, porro
-        doloremque voluptate, animi excepturi doloribus minima ea perferendis!
-        Minima odit enim officiis commodi iste nostrum ipsam placeat! Rerum
-        reprehenderit molestiae ut sint modi? Vel, esse suscipit. Ducimus
-        quaerat omnis ullam? Labore corrupti laudantium dicta assumenda itaque
-        vitae voluptas aliquid ratione ducimus accusamus unde quas provident,
-        commodi a? Architecto incidunt mollitia quam est, nisi sit, esse ipsam
-        alias eius consequuntur neque. Soluta eos quisquam cumque ad esse
-        officiis, deserunt vitae autem nisi placeat quos itaque fugiat. Quis,
-        unde!
-      </p>
+      <h2>Buttons</h2>
+      <h3>Default, green, red, purple, disabled</h3>
+      <Button value={"Hola"} href={"https://google.es"} />
+      <Button value={"Hola"} href={"https://google.es"} color={"green"} />
+      <Button value={"Hola"} href={"https://google.es"} color={"red"} />
+      <Button value={"Hola"} href={"https://google.es"} color={"purple"} />
+      <Button value={"Hola"} href={"https://google.es"} disabled={true} />
+
+      <h2>Loader</h2>
+
+      <Loader />
     </div>
   );
 };
